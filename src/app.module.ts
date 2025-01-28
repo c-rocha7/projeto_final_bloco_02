eb9eb3ffe424c7c3df11095136dcb850fd160041
Cauã R. Pereira <7aauac@gmail.com>
2025-01-28 11:18:06 -0300

    :card_file_box: raw: Realizada a configuração da conexão com o banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql',
+      port: 3306,
+      username: 'app_user',
+      password: 'app_password',
+      database: 'db_farmacia',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
